ee90aa989d7ed71b920e3c328e6a8f17281f6b1f
rudresh31 <rudrapandey170@gmail.com>
2024-07-05 21:19:39 +0530

    add some standard comments

--- test/pageobject/login.page.js
+++ test/pageobject/login.page.js
@@ -1,10 +1,29 @@
 class LoginPage {
+    // get burger Icon by xpath
+    get BurgerIcon() { return $('//*[@content-desc="open menu"]/android.widget.ImageView'); }
+    // get lohin link by xpath
+    get loginlink() { return $('//*[@text="Log In"]'); }
+    // get user name field by accessibility id
     get username() { return $('~Username input field'); }
+    // get password field by accessibility id
     get password() { return $('~Password input field'); }
+    // get login btn by accessibility id
     get loginBtn() { return $('~Login button'); }
+    // get welcome message text by xpath
     get welcomeMsg() { return $('//android.widget.TextView[@text="Products"]'); }
+    // get warning message text by xpath
     get warningcomeMsg() { return $('//*[@text="Provided credentials do not match any user in this service."]'); }
 
+    async open() {
+        // opening app and launching activity
+        await browser.url('apps\Android-MyDemoAppRN.1.3.0.build-244.apk');
+        // click on burgor Icon
+        await this.BurgerIcon.click()
+        // click on login link 
+        await this.loginlink.click()
+
+        
+    }
 }
 
 module.exports = new LoginPage();
--- test/specs/login.spec.js
+++ test/specs/login.spec.js
@@ -3,21 +3,29 @@ const LoginPage = require('../pageobjects/login.page');
 const assert = require('chai').assert;
 
 Given(/^I open the login page$/, async () => {
+    // opening app
     await LoginPage.open();
 });
 
 When(/^I login with "([^"]*)" and "([^"]*)"$/, async (username, password) => {
+    // Entering Username
     await LoginPage.username.setValue(username);
+    // Entering Password
     await LoginPage.password.setValue(password);
+    // clicking on login btn
     await LoginPage.loginBtn.click();
 });
 
 Then(/^I should see the "([^"]*)"$/, async (message) => {
+    // get welcome message
     const welcomeText = await LoginPage.welcomeMsg.getText();
+    // asserting Text
     assert.strictEqual(welcomeText, message);
 });
 
 Then(/^I should see the warning meaasge "([^"]*)"$/, async (message) => {
+    // get Warning message
     const Text = await LoginPage.warningcomeMsg.getText();
+    // asserting Text
     assert.strictEqual(Text, message);
 });
